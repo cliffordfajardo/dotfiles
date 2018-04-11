ca32c7a8d9972c9a2f1ec628c634ab2a41f2a1fc
Clifford Fajardo <cliffordfajardo@users.noreply.github.com>
2018-04-11 14:19:49 -0700

    add more npm packages
    - add fkill 
    - diff-so-fancy

--- .007npm-setup.sh
+++ .007npm-setup.sh
@@ -1,11 +1,15 @@
+# Smart Emoji finder
 npm install -g alfred-emoj
+# fancy man pages
 npm install -g tldr
 # Type `git open` to open the GitHub page or website for a repository.
 npm install -g git-open
 # fancy listing of recent branches
 npm install -g git-recent
-
-
+# git diff command .. with fancy human readable output
+npm i -g diff-so-fancy
+# kill procceses cross platform (useful for non-mac setups)
+npm install -g fkill-cli
 
 echo "COMPLETED '007npmsetup.sh' script"
 echo "------------------------------------------"
