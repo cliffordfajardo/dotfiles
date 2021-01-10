9efad1e3a1e4d328d1da6d073f306f42cd4cdbe7
Clifford Fajardo <cliffordfajardo@users.noreply.github.com>
2021-01-10 15:15:56 -0800

    Create 010-non-brew-installs.sh

---
+++ 010-non-brew-installs.sh
@@ -0,0 +1,4 @@
+# install deno curl -fsSL https://deno.land/x/install/install.sh | sh
+# After installing deno with curl, stdout of command mentioned adding the following to exports:
+export DENO_INSTALL="/Users/cliffordfajardo/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
