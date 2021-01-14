1629e6c5449beed7c179659fa536819b62e460a9
Clifford Fajardo <cliffordfajardo@users.noreply.github.com>
2021-01-13 20:45:34 -0800

    add webstorm like parameter hints & highlightlines

--- .006vscode-setup.sh
+++ .006vscode-setup.sh
@@ -9,20 +9,21 @@ code --install-extension formulahendry.auto-close-tag
 code --install-extension xyz.local-history
 code --install-extension formulahendry.auto-rename-tag
 code --install-extension CoenraadS.bracket-pair-colorizer
-code --install-extension tcasey.caddy-vscode
 code --install-extension EditorConfig.editorconfig
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension eamodio.gitlens
 code --install-extension codezombiech.gitignore
 code --install-extension rsbondi.highlight-words
 code --install-extension wix.vscode-import-cost
-code --install-extension wmaurer.vscode-jumpy
 code --install-extension monokai.theme-monokai-pro-vscode
 code --install-extension leizongmin.node-module-intellisense
 code --install-extension techer.open-in-browser
-code --install-extension christian-kohler.path-intellisense
 code --install-extension rafamel.subtle-brackets
 code --install-extension robertohuertasm.vscode-icons
+code --install-extension DominicVonk.parameter-hints
+code --install-extension cliffordfajardo.highlight-line-vscode
+# Themes
+code --install-extensionn orhun.black-waves	
 
 echo "COMPLETED '006vscode-setup.sh' script"
 echo "------------------------------------------"
